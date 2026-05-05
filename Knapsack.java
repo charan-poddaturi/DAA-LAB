3a88377fcc33339b906b2a8e8adc2d9c7ecbcbf2
charan poddaturi <24r11a6735@gcet.edu.in>
2026-05-05 18:15:42 +0530

    Replace Kruskal's algorithm with knapsack implementation

--- Knapsack.java
+++ Knapsack.java
@@ -1,59 +1,57 @@
-import java.util.*;
-class Kruskal {
-    int V;
-    static class Edge implements Comparable<Edge> {
-        int u, v, weight;
-        public int compareTo(Edge other) {
-            return this.weight - other.weight;
-        }
+import java.util.Scanner;
+
+public class Knapsack {
+
+    static int knapsack(int[] w, int[] p, int n, int W) {
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j <= W; j++)
+                dp[i][j] = (w[i-1] <= j)
+                    ? Math.max(dp[i-1][j], dp[i-1][j - w[i-1]] + p[i-1])
+                    : dp[i-1][j];
+
+        return dp[n][W];
     }
-    static int find(int[] parent, int i) {
-        if (parent[i] == i)
-            return i;
-        return find(parent, parent[i]);
-    }
-    static void union(int[] parent, int x, int y) {
-        int xset = find(parent, x);
-        int yset = find(parent, y);
-        parent[xset] = yset;
-    }
-    void kruskalMST(int[][] graph) {
-        // Extract edges from adjacency matrix (upper triangle only, avoid duplicates)
-        List<Edge> edgeList = new ArrayList<>();
-        for (int i = 0; i < V; i++)
-            for (int j = i + 1; j < V; j++)
-                if (graph[i][j] != 0) {
-                    Edge e = new Edge();
-                    e.u = i; e.v = j; e.weight = graph[i][j];
-                    edgeList.add(e);
-                }
-        Edge[] edges = edgeList.toArray(new Edge[0]);
-        Arrays.sort(edges);
-        int[] parent = new int[V];
-        for (int i = 0; i < V; i++)
-            parent[i] = i;
-        System.out.println("Edge \tWeight");
-        for (Edge edge : edges) {
-            int x = find(parent, edge.u);
-            int y = find(parent, edge.v);
-            if (x != y) {
-                System.out.println(edge.u + " - " + edge.v + "\t" + edge.weight);
-                union(parent, x, y);
+
+    static void traceback(int[] w, int[] p, int n, int W) {
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j <= W; j++)
+                dp[i][j] = (w[i-1] <= j)
+                    ? Math.max(dp[i-1][j], dp[i-1][j - w[i-1]] + p[i-1])
+                    : dp[i-1][j];
+
+        // Traceback to find selected items
+        System.out.print("Items selected (1-indexed): ");
+        int j = W;
+        for (int i = n; i > 0; i--)
+            if (dp[i][j] != dp[i-1][j]) {
+                System.out.print(i + " ");
+                j -= w[i-1];
             }
-        }
+        System.out.println();
+        System.out.println("Max Profit: " + dp[n][W]);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of vertices: ");
-        int V = sc.nextInt();
-        int[][] graph = new int[V][V];
-        System.out.println("Enter adjacency matrix (" + V + "x" + V + "):");
-        for (int i = 0; i < V; i++)
-            for (int j = 0; j < V; j++)
-                graph[i][j] = sc.nextInt();
-        Kruskal k = new Kruskal();
-        k.V = V;
-        k.kruskalMST(graph);
+        System.out.print("Enter number of items: ");
+        int n = sc.nextInt();
+        System.out.print("Enter knapsack capacity: ");
+        int W = sc.nextInt();
+
+        int[] w = new int[n], p = new int[n];
+        System.out.println("Enter weight and profit for each item:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Item " + (i+1) + " -> Weight: ");
+            w[i] = sc.nextInt();
+            System.out.print("         Profit: ");
+            p[i] = sc.nextInt();
+        }
+
+        traceback(w, p, n, W);
         sc.close();
     }
 }
