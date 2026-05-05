9e8d70c7d073a0869d9d85bd55dec2244e222d26
charan poddaturi <24r11a6735@gcet.edu.in>
2026-05-05 22:27:14 +0530

    Refactor OBST.java for dynamic array sizing

--- OBST.java
+++ OBST.java
@@ -2,92 +2,65 @@ import java.util.Scanner;
 
 public class OBST {
 
-    private static final int MAX = 10;
-
     public static void main(String[] args) {
-
-        int[][] w = new int[MAX][MAX];
-        int[][] c = new int[MAX][MAX];
-        int[][] r = new int[MAX][MAX];
-
-        int[] p = new int[MAX];
-        int[] q = new int[MAX];
-
-        int rt = 0, min, min1, n;
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of elements: ");
-        n = scanner.nextInt();
+        int n = sc.nextInt();
+
+        int[] p = new int[n + 1];
+        int[] q = new int[n + 1];
+        int[][] w = new int[n + 1][n + 1];
+        int[][] c = new int[n + 1][n + 1];
+        int[][] r = new int[n + 1][n + 1];
 
         System.out.println("Enter the Probabilities of Successful searches:");
         for (int i = 1; i <= n; i++) {
             System.out.print("p" + i + ": ");
-            p[i] = scanner.nextInt();
+            p[i] = sc.nextInt();
         }
 
         System.out.println("Enter the Probabilities of Unsuccessful Searches:");
         for (int i = 0; i <= n; i++) {
             System.out.print("q" + i + ": ");
-            q[i] = scanner.nextInt();
+            q[i] = sc.nextInt();
         }
 
+        // Initialize diagonal
         System.out.println("\nW\t\tC\t\tR");
-
-        // Initialize
         for (int i = 0; i <= n; i++) {
             w[i][i] = q[i];
-            c[i][i] = 0;
-            r[i][i] = 0;
-
-            System.out.printf(
-                "W[%d][%d]: %d\tC[%d][%d]: %d\tR[%d][%d]: %d\n",
-                i, i, w[i][i],
-                i, i, c[i][i],
-                i, i, r[i][i]
-            );
+            System.out.printf("W[%d][%d]: %d\tC[%d][%d]: %d\tR[%d][%d]: %d\n",
+                i, i, w[i][i], i, i, c[i][i], i, i, r[i][i]);
         }
 
+        // Fill tables
         System.out.println();
-
-        // OBST calculation
         for (int m = 1; m <= n; m++) {
-
             for (int i = 0; i <= n - m; i++) {
-
                 int j = i + m;
 
                 w[i][j] = p[j] + q[j] + w[i][j - 1];
 
-                min = Integer.MAX_VALUE;
-
+                int min = Integer.MAX_VALUE;
                 for (int k = i + 1; k <= j; k++) {
-
-                    min1 = c[i][k - 1] + c[k][j] + w[i][j];
-
-                    if (min1 < min) {
-                        min = min1;
-                        rt = k;
+                    int cost = c[i][k - 1] + c[k][j] + w[i][j];
+                    if (cost < min) {
+                        min = cost;
+                        r[i][j] = k;
                     }
                 }
-
                 c[i][j] = min;
-                r[i][j] = rt;
 
-                System.out.printf(
-                    "W[%d][%d]: %d\tC[%d][%d]: %d\tR[%d][%d]: %d\n",
-                    i, j, w[i][j],
-                    i, j, c[i][j],
-                    i, j, r[i][j]
-                );
+                System.out.printf("W[%d][%d]: %d\tC[%d][%d]: %d\tR[%d][%d]: %d\n",
+                    i, j, w[i][j], i, j, c[i][j], i, j, r[i][j]);
             }
-
             System.out.println();
         }
 
         System.out.println("Minimum cost = " + c[0][n]);
         System.out.println("Root = " + r[0][n]);
 
-        scanner.close();
+        sc.close();
     }
 }
