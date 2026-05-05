44bd7f4da3a15a1fe652cbcec5efea0f5706773f
charan poddaturi <24r11a6735@gcet.edu.in>
2026-05-05 19:10:54 +0530

    Refactor Kruskal's algorithm implementation

--- Kruskal.java
+++ Kruskal.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Job {
+public class Kruskal {
 
     static int[] parent;
 
@@ -10,69 +10,57 @@ public class Job {
         return i;
     }
 
-    static void union(int u, int v) {
-        parent[v] = u;
-    }
-
-    static void jobScheduling(int[] id, int[] deadline, int[] profit, int n) {
-
-        // Sort jobs by profit descending (bubble sort)
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (profit[j] < profit[j + 1]) {
-                    int tmp;
-                    tmp = profit[j];   profit[j] = profit[j+1];   profit[j+1] = tmp;
-                    tmp = deadline[j]; deadline[j] = deadline[j+1]; deadline[j+1] = tmp;
-                    tmp = id[j];       id[j] = id[j+1];             id[j+1] = tmp;
-                }
-            }
-        }
-
-        // Find max deadline
-        int maxDeadline = 0;
-        for (int i = 0; i < n; i++)
-            if (deadline[i] > maxDeadline)
-                maxDeadline = deadline[i];
-
-        // Initialize parent array
-        parent = new int[maxDeadline + 1];
-        for (int i = 0; i <= maxDeadline; i++)
-            parent[i] = i;
-
-        int totalProfit = 0;
-
-        System.out.println("Scheduled Jobs:");
-        for (int i = 0; i < n; i++) {
-            int availableSlot = find(deadline[i]);
-            if (availableSlot > 0) {
-                System.out.println("Job " + id[i] + " scheduled at slot " + availableSlot);
-                totalProfit += profit[i];
-                union(find(availableSlot - 1), availableSlot);
-            }
-        }
-
-        System.out.println("Total Profit: " + totalProfit);
+    static void union(int i, int j) {
+        parent[find(j)] = find(i);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of jobs: ");
+        System.out.print("Enter number of vertices: ");
         int n = sc.nextInt();
 
-        int[] id       = new int[n];
-        int[] deadline = new int[n];
-        int[] profit   = new int[n];
+        int[][] cost = new int[n][n];
 
-        System.out.println("Enter id, deadline, profit for each job:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Job " + (i + 1) + ": ");
-            id[i]       = sc.nextInt();
-            deadline[i] = sc.nextInt();
-            profit[i]   = sc.nextInt();
+        System.out.println("Enter cost adjacency matrix (use 9999 for no edge):");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cost[i][j] = sc.nextInt();
+
+        // Each node is its own parent initially
+        parent = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        int mincost = 0;
+        int edgesAdded = 0;
+
+        System.out.println("\nEdges in MST:");
+
+        // Keep picking minimum cost edge until n-1 edges added
+        while (edgesAdded < n - 1) {
+
+            // Find minimum cost edge that doesn't form a cycle
+            int min = 9999;
+            int u = 0, v = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (find(i) != find(j) && cost[i][j] < min) {
+                        min = cost[i][j];
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+
+            // Add edge to MST
+            union(u, v);
+            System.out.println(u + " - " + v + " : " + cost[u][v]);
+            mincost += cost[u][v];
+            edgesAdded++;
         }
 
-        jobScheduling(id, deadline, profit, n);
+        System.out.println("Minimum Cost = " + mincost);
         sc.close();
     }
 }
